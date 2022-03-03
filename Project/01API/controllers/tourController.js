f9bf8b306a7bff02367d617d95fed16650528008
mydevth <mydevth@chonburi.biz>
2022-03-03 23:06:15 +0700

    030322-2

--- Project/01API/controllers/tourController.js
+++ Project/01API/controllers/tourController.js
@@ -1,6 +1,7 @@
 //  tourController.js
 
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -14,60 +15,13 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
-    // BUILD QUERY
-    // 1A)   Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // {difficulty:'easy', duration:{$gte:5}  }
-    // { duration: { gte: '5' }, difficulty: 'easy' }
-    // gte, gt, lte, lt
-
-    // 2) Sorting
-    console.log(`req.query.sort = ${req.query.sort}`);
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select('name duration price');
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    // IDEA - page=2&limit=10, 1-10, page 1, 11-20, page2 21-30 page3 31-40
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    //  show error when request page number more than exist
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');  //not work ,not show
-    }
-
-
-
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //  way 1 to do query  
     // const tours = await Tour.find({
