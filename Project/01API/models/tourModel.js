39e18fbd80e1099eaf638e6e24a252f79a0daaf5
mydevth <mydevth@chonburi.biz>
2022-03-05 23:12:52 +0700

    107BuildinVaridation

--- Project/01API/models/tourModel.js
+++ Project/01API/models/tourModel.js
@@ -7,7 +7,9 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters']
   },
 
   slug: String,
@@ -23,14 +25,17 @@ const tourSchema = new mongoose.Schema({
   difficulty: {
     type: String,
     required: [true, 'A tour must have a difficulty'],
-    // enum: {
-    //   values: ['easy', 'medium', 'difficult'],
-    //   message: 'Difficulty is either: easy, medium, difficult'
-    // }
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be under 5.0']
+
   },
   ratingsQuantity: {
     type: Number,
@@ -108,6 +113,12 @@ tourSchema.post(/^find/, function (docs, next) {       // regular expression , a
   next();
 });
 
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 
 
 
