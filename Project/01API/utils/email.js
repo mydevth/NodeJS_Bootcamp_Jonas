1dbde04d09fc75f25bb6480f6a645c63ba558000
mydevth <mydevth@chonburi.biz>
2022-03-16 22:10:31 +0700

    136 Sending email with Nodemailer WORKgit add .

--- Project/01API/utils/email.js
+++ Project/01API/utils/email.js
@@ -5,7 +5,7 @@ const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
 
     host: process.env.EMAIL_HOST,
-    port: porcess.env.EMAIL_PORT,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
@@ -13,17 +13,18 @@ const sendEmail = async options => {
     // Activate in gmail "less secure app" option
   });
 
-  // 2) Define the mail option
+  // 2) Define the email options
   const mailOptions = {
     from: 'myDevTH <mydevth@hotmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
-    // html: 
-  }
+    // html:
+  };
 
-  // 3) Actually send the email with nodemailer
+  // 3) Actually send the email
   await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
+
