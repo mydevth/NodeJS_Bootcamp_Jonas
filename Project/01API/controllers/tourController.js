37c2a19fe9e073ba53db15ead8aa868697d68a08
mydevth <mydevth@chonburi.biz>
2022-03-25 23:40:40 +0700

    153 Populating Tour Guides

--- Project/01API/controllers/tourController.js
+++ Project/01API/controllers/tourController.js
@@ -34,6 +34,8 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+
   // Tour.findOne({ _id: req.params.id })
 
   if (!tour) {
