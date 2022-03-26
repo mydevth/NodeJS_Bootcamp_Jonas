57cac7c9b79f266e1a03f466a03c1921f3862372
mydevth <mydevth@chonburi.biz>
2022-03-26 23:27:07 +0700

    155 Creating and Getting Reviews

--- Project/01API/app.js
+++ Project/01API/app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 //   import routes file
 const tourRouter = require('./routes/tourRoutes');    // no need .js
 const userRouter = require('./routes/userRoutes');    // no need .js
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -72,6 +73,7 @@ app.use((req, res, next) => {
 // 3) ROUTES /MOUNT   - SOMEMOVE TO FILE
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
