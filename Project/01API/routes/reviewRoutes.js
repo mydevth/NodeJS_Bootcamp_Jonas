6c783418b0aa3611ea3cb1cad1b6930c914e3d4e
mydevth <mydevth@chonburi.biz>
2022-03-27 21:52:26 +0700

    159 Nested Routes with Express

--- Project/01API/routes/reviewRoutes.js
+++ Project/01API/routes/reviewRoutes.js
@@ -3,7 +3,10 @@ const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+// POST /tour/tourID/reviews    (mergeParams)
+// POST /reviews
 
 router
   .route('/')
--- Project/01API/routes/tourRoutes.js
+++ Project/01API/routes/tourRoutes.js
@@ -3,11 +3,18 @@ const express = require('express');
 
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+// const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);    //   param  (checkID from URL ,call at tourController.js), 
+
+// POST /tour/tourID/reviews
+// GET /tour/tourID/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
@@ -31,13 +38,6 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour);
 
-// POST /tour/tourID/reviews
-// GET /tour/tourID/reviews
-// GET /tour/tourID/reviews/reviewID
-router.route('/:tourId/reviews')
-  .post(authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview);
 
 
 module.exports = router;    
