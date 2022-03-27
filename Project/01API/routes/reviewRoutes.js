6aea3258ac7a05d90fbf6465880b8544d71c6638
mydevth <mydevth@chonburi.biz>
2022-03-27 22:10:03 +0700

    160 Adding a Nested GET Endpoint

--- Project/01API/routes/reviewRoutes.js
+++ Project/01API/routes/reviewRoutes.js
@@ -6,6 +6,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router({ mergeParams: true });
 
 // POST /tour/tourID/reviews    (mergeParams)
+// GET /tour/tourID/reviews    
 // POST /reviews
 
 router
