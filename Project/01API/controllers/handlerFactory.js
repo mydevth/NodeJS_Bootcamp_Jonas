3a02e8b22e0a1b473f0d51f67fd3c28a44e875fd
mydevth <mydevth@chonburi.biz>
2022-03-28 22:36:34 +0700

    162 Factory Functions : Update and Create

--- Project/01API/controllers/handlerFactory.js
+++ Project/01API/controllers/handlerFactory.js
@@ -15,3 +15,33 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     data: null
   });
 });
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+
+  const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true    // enable validations
+  });
+
+  if (!doc) {
+    return next(new AppError('No document found widh this ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc
+    }
+  });
+});
+
+exports.createOne = Model => catchAsync(async (req, res, next) => {
+  const doc = await Model.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      data: doc
+    }
+  });
+});
