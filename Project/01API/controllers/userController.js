96cdbedc08df03e9e5e1d2efaa5c9a816f76c1f3
mydevth <mydevth@chonburi.biz>
2022-03-29 22:35:30 +0700

    163 Factory Functions : Reading

--- Project/01API/controllers/userController.js
+++ Project/01API/controllers/userController.js
@@ -3,7 +3,6 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach(el => {
@@ -12,23 +11,9 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 }
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
-
     return next(new AppError('This route is not for password updates. Please user /updateMyPassword', 400));
   }
 
@@ -38,7 +23,6 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // 3) Update user document
   // body.role: 'admin'
   const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
-
 
   res.status(200).json({
     status: 'success',
@@ -57,23 +41,17 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not defined! Please use /signup instead'
   });
 };
 
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
 // Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
-
 exports.deleteUser = factory.deleteOne(User);
 // exports.deleteUser = (req, res) => {
 //   res.status(500).json({
--- Project/01API/routes/reviewRoutes.js
+++ Project/01API/routes/reviewRoutes.js
@@ -19,7 +19,9 @@ router
     reviewController.createReview
   );
 
-router.route('/:id')
+router
+  .route('/:id')
+  .get(reviewController.getReview)
   .patch(reviewController.updateReview)
   .delete(reviewController.deleteReview);
 
