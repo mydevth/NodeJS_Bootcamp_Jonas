ab415ec3f555459575eaf822722dc8dfafc9858a
mydevth <mydevth@chonburi.biz>
2022-03-30 22:17:28 +0700

    165 Adding Missing Authentication and Authorization

--- Project/01API/routes/reviewRoutes.js
+++ Project/01API/routes/reviewRoutes.js
@@ -2,18 +2,14 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-
 const router = express.Router({ mergeParams: true });
 
-// POST /tour/tourID/reviews    (mergeParams)
-// GET /tour/tourID/reviews    
-// POST /reviews
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -22,8 +18,8 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+  .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview);
 
 module.exports = router;
 
