8db9b0d8be7652f87c781ff77b7fd6794ca7d12f
mydevth <mydevth@chonburi.biz>
2022-04-06 02:47:32 +0700

    181 Setting up the Project Structure

--- Project/01API/app.js
+++ Project/01API/app.js
@@ -16,6 +16,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');    // no need .js
 const userRouter = require('./routes/userRoutes');    // no need .js
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -76,25 +77,7 @@ app.use((req, res, next) => {
 // 2) HANDERS  - MOVE TO FILE
 
 // 3) ROUTES /MOUNT   - SOMEMOVE TO FILE
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Akkadate'
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
