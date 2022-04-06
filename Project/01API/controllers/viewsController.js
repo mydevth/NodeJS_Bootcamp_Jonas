dcce8ffd5e9ee8e3a58f1ae9216e0df685b4a66d
mydevth <mydevth@chonburi.biz>
2022-04-06 16:37:13 +0700

    186 Including a Map with Mapbox Part1

--- Project/01API/controllers/viewsController.js
+++ Project/01API/controllers/viewsController.js
@@ -23,9 +23,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
   // 2) Build template
   // 3) Render template using data from 1)
-
   res.status(200).render('tour', {
-    title: `${tour.name}`,
+    title: `${tour.name} Tour`,
     tour
   });
 });
