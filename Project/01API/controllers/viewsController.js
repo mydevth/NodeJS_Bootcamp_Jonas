47edd6a216966a891286216b953e031e3e0c7326
mydevth <mydevth@chonburi.biz>
2022-04-09 08:50:49 +0700

    193 Rendering error page

--- Project/01API/controllers/viewsController.js
+++ Project/01API/controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -21,6 +22,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
   });
 
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
   // 2) Build template
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
@@ -28,6 +33,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
     tour
   });
 });
+
+
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
