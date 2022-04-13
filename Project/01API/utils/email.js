ccd94ffb4c51a75031ef6f54357e609b808dc8fd
mydevth <mydevth@chonburi.biz>
2022-04-13 12:05:27 +0700

    208 Sending Passowrd Reset Emails

--- Project/01API/utils/email.js
+++ Project/01API/utils/email.js
@@ -61,6 +61,12 @@ module.exports = class Email {
   async sendPasswordReset() {
     await this.send(
       'passwordReset',
+      'Your password reset token (valid for only 10 minutes)');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
       'Your password reset token (valid for only 10 minutes)'
     );
   }
