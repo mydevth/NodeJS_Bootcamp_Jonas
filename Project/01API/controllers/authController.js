6a5921e1e3fab7a9e8d007290689feede5e95af4
mydevth <mydevth@chonburi.biz>
2022-04-13 08:18:05 +0700

    207 Email Templates with Pug : Welcome Emails

--- Project/01API/controllers/authController.js
+++ Project/01API/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 
 const signToken = id => {
@@ -37,29 +37,45 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  // const newUser = await User.create(req.body);   //***** code correct by Jonas */
+// exports.signup = catchAsync(async (req, res, next) => {
+//   // const newUser = await User.create(req.body);   //***** code correct by Jonas */
 
-  const newUser = await User.create({      //***** code correct by Jonas */
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
-    role: req.body.role
-  });
+//   const newUser = await User.create({      //***** code correct by Jonas */
+
+//     const url = 0;
+//     await new Email(newUser, url).sendWelcome();
+
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password,
+//     passwordConfirm: req.body.passwordConfirm,
+//     passwordChangedAt: req.body.passwordChangedAt,
+//     role: req.body.role
+//   });
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
-  // const token = signToken(newUser._id);
-
-  // res.status(201).json({
-  //   status: 'succes',
-  //   token,
-  //   data: {
-  //     user: newUser
-  //   }
-  // });
 });
+
+
+
+// createSendToken(newUser, 201, res);
+//   // const token = signToken(newUser._id);
+
+//   // res.status(201).json({
+//   //   status: 'succes',
+//   //   token,
+//   //   data: {
+//   //     user: newUser
+//   //   }
+//   // });
+// });
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
@@ -191,11 +207,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message
+    // });
 
     res.status(200).json({
       status: 'success',
--- Project/01API/utils/email_org.js
+++ Project/01API/utils/email_org.js
@@ -7,21 +7,14 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Akkadate Siripongwattana <${process.env.EMAIL_FROM}>`;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD
-        }
-      });
+      return 1;
     }
-
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
@@ -57,11 +50,8 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
   }
-
-  async sendPasswordReset() {
-    await this.send(
-      'passwordReset',
-      'Your password reset token (valid for only 10 minutes)'
-    );
-  }
 };
+
+
+
+
